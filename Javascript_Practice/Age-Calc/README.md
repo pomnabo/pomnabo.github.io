912493f61313da48a0aff44620ec2e0c6ebff87d
pomnabo <pomnabo@yahoo.com>
2024-06-29 01:41:30 -0700

    adding completed age calculator project.

--- Javascript_Practice/Age-Calc/README.md
+++ Javascript_Practice/Age-Calc/README.md
@@ -7,13 +7,22 @@ Overview:
 
 This is 10 of 10 (Last one of this set!) small javascript project tutorials featured in Vijay Kumar's Udemy course. In this tutorial, the viewer is guided through writing comprehensive age calculator.
 
+Overall, these exercises were great practice. I don't think I'm quite ready yet to produce my own javascript code, but alas, in due time.
+
 ------------------------------
 Coding Challenges:
 
+I decided to change the input type of the text box from "text" to "date." I felt this was more intuitive, and also supplies a nice calendar gui for easier user entry. As the tutorial went on, I had to sort of constantly adjust what I was doing based on what he was doing, to account for the different input type.
 
+Other than that, I didn't understand much (if any) of the validation code; so I've added it to my list of things to review.
 
 
 ------------------------------
 Other Notes:
 
-I decided to change the input type of the text box from "text" to "date." I felt this was more intuitive, and also supplies a nice calendar gui for easier user entry.
+I realize that the instructor chose to keep the input type as text (rather than date) likely because he wanted to demonstrate the use of validators in javascript. While I can appreciate that, I also want to honor my own knowledge in design principles, and maintain my initial instinct to designate the type as "date." Still, I went ahead and wrote out the code for the validation to make sure I get that practice too. In the end, I had to comment out the validater code, because I wasn't sure how to adjust it to account for my use of a date input.
+
+I also liked challenging myself to adapt my code alongside the tutorial.
+
+While declaring the many variables in this tutorial, I thought it would be fun to add another variable (or even a few) to show the number of 
+
--- Javascript_Practice/Age-Calc/js/script.js
+++ Javascript_Practice/Age-Calc/js/script.js
@@ -1,6 +1,94 @@
 function calculateAge(){
 
-    const ageResult = document.getElementById("")
+    const todayDate = new Date();
+    const bdayInput = document.getElementById("birthdate").value;
+    const bdayParts = bdayInput.split("-");
+    const bdayYear = bdayParts[0];
+    const bdayMonth = bdayParts[1];
+    const bdayDay = bdayParts[2];
+    const bdayDate = new Date(bdayYear, bdayMonth, bdayDay);
+
+/*
+    const isValidDate = (date) =>{
+
+        return(
+            Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)
+        );
+    };
+    
+
+    if(!isValidDate(bdayDate)){
+
+        alert("Invalid Date Format. Please Enter a Valid Date in DD/MM/YYYY");
+        return;
+    }
+*/
+    const ageInMillSec = todayDate - bdayDate;
+    const ageInSeconds = Math.floor(ageInMillSec / 1000);
+    const ageInMinutes = Math.floor(ageInSeconds / 60);
+    const ageInHours = Math.floor(ageInMinutes / 60);
+    const ageInDays = Math.floor(ageInHours / 24);
+    const ageInWeeks = Math.floor(ageInDays / 7);
+    const ageInMonths = Math.floor(ageInDays / 30.436875); //this is an approximation of days in a month.
+    const ageInYears = Math.floor(ageInDays / 365.25); //this is an approximation to account for leap years.
+
+
+    const resultContainer = document.getElementById("resultContainer");
+    const ageResult = document.getElementById("ageResults");
+
+    ageResult.innerHTML = `
+                <div class="results-item">
+                    <h3>Your Exact Age:</h3>
+                    <p>${ageInYears} Years ${ageInMonths % 12} Months ${ageInDays % 30} Days</p>
+                </div>
+                <div class="results-item">
+                    <h3>Months Passed:</h3>
+                    <p>${ageInMonths}</p>
+                </div>
+                <div class="results-item">
+                    <h3>Weeks Passed:</h3>
+                    <p>${ageInWeeks}</p>
+                </div>
+                <div class="results-item">
+                    <h3>Days Passed:</h3>
+                    <p>${ageInDays}</p>
+                </div>
+                <div class="results-item">
+                    <h3>Hours Passed:</h3>
+                    <p>${ageInHours}</p>
+                </div>
+                <div class="results-item">
+                    <h3>Minutes Passed:</h3>
+                    <p>${ageInMinutes}</p>
+                </div>
+                <div class="results-item">
+                    <h3>Seconds Passed:</h3>
+                    <p>${ageInSeconds}</p>
+                </div>
+                <div class="results-item">
+                    <h3>Milliseconds Passed:</h3>
+                    <p>${ageInMillSec}</p>
+                </div>
+    `;
+
+    resultContainer.style.display = "block";
+
+
+    console.log(bdayInput);
+    console.log(bdayParts);
+    console.log(bdayDay);
+    console.log(bdayMonth);
+    console.log(bdayYear);
+
 
 
 }
+
+const ageCalcForm = document.getElementById("ageCalculator");
+ageCalcForm.addEventListener("submit",(event)=>{
+
+    event.preventDefault();
+    calculateAge();
+
+});
+
