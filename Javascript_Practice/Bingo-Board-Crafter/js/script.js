67f187b785e4c25b4e194dd12b1cb81c0b253cdf
pomnabo <pomnabo@yahoo.com>
2024-09-29 20:23:09 -0700

    added the function to create the popup overlay where the bingo board will generate.

--- Javascript_Practice/Bingo-Board-Crafter/js/script.js
+++ Javascript_Practice/Bingo-Board-Crafter/js/script.js
@@ -11,7 +11,9 @@ const bingoBoard = document.getElementById("bingoPreview");
 
 function bingoBoardFinal() {
 
-    const freeSwitchValue = document.getElementById("freeSwitch").checked;
+    document.getElementById("bingoPreview").style.display = "block";
+
+    const freeSwitchValue = document.getElementById("freeSwitch").checked; //MUST be present inside the function to get the checkbox value when the button is called.
     console.log(freeSwitchValue);
 
     /* when this function is called, it will generate a grid of squares based on the criteria */
@@ -19,3 +21,8 @@ function bingoBoardFinal() {
 
 const bingoBtn = document.getElementById("makeBingoBtn");
 bingoBtn.addEventListener('click', bingoBoardFinal);
+
+
+function previewOff() {
+    document.getElementById("bingoPreview").style.display = "none";
+}
