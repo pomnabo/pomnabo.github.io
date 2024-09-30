432deee8fbbe776bf5a8c3b9b316e99c443f14f8
pomnabo <pomnabo@yahoo.com>
2024-09-29 20:07:16 -0700

    figured out how to get the value of the toggle switch and have it called every time the button is pushed.

--- Javascript_Practice/Bingo-Board-Crafter/js/script.js
+++ Javascript_Practice/Bingo-Board-Crafter/js/script.js
@@ -1,28 +1,21 @@
 
-/* function genBingoBoard() {
-    
 
+const gridSize = document.getElementById("grid-size").value;
+const promptsInput = document.getElementById("promptsField").value;
+
+
+
+const bingoBoard = document.getElementById("bingoPreview");
+
+
+
+function bingoBoardFinal() {
+
+    const freeSwitchValue = document.getElementById("freeSwitch").checked;
+    console.log(freeSwitchValue);
+
+    /* when this function is called, it will generate a grid of squares based on the criteria */
 }
 
-
-
-const genBingoBtn = document.querySelector('makeBingoBtn');
-genBingoBtn.addEventListener("click", genBingoBoard); */
-
-
-const squareSize = document.getElementById("grid-size").value;
-
-let freeSwitchValue;
-document.getElementById("freeSwitch").value = "freeSwitchValue";
-
-
-let promptTextArea;
-const bingoPrompts = document.getElementById("promptsField");
-
-
-const genBingoBtn = document.getElementById("makeBingoBtn");
-genBingoBtn.addEventListener('click', function genBingoBoard() {
-    
-    promptTextArea = bingoPrompts.value;
-
-})
+const bingoBtn = document.getElementById("makeBingoBtn");
+bingoBtn.addEventListener('click', bingoBoardFinal);
