c0834f2ff426f8d6e34674afe15141fbd51164f3
pomnabo <pomnabo@yahoo.com>
2024-10-01 01:46:49 -0700

    added a popup overlay alert message when one or more fields is empty

--- Javascript_Practice/Bingo-Board-Crafter/js/script.js
+++ Javascript_Practice/Bingo-Board-Crafter/js/script.js
@@ -1,9 +1,8 @@
 
-
+const bingoTitle = document.getElementById("bingoTitle");
+const boardDiv = document.getElementById("brdSqDiv");
 
 function bingoBoardFinal() { /* when this function is called, it will generate a grid of squares based on the criteria */
-
-    document.getElementById("bingoPreview").style.display = "block";//turns on the popup preview overlay when the 'generate' button is clicked
 
     const gridSize = document.getElementById("grid-size").value;// variables MUST be present inside the function to get the input value when the button is called.
     const freeSwitchValue = document.getElementById("freeSwitch").checked;
@@ -13,10 +12,23 @@ function bingoBoardFinal() { /* when this function is called, it will generate a
 
     console.log(gridSize, freeSwitchValue, titleValue, promptsInput);
 
-    const bingoTitle = document.getElementById("bingoTitle");
-    bingoTitle.innerHTML = titleValue;
+    if(gridSize === "" || freeSwitchValue === "" || titleValue === "" || promptsInput === ""){
 
-    
+        document.getElementById("bingoPreview").style.display = "block";//turns on the popup preview overlay when the 'generate' button is clicked
+        bingoTitle.innerHTML = "Whoopsiedoodle!";
+        boardDiv.innerHTML = "You forgot to fill something in."
+
+
+    }
+    else {
+
+        document.getElementById("bingoPreview").style.display = "block";//turns on the popup preview overlay when the 'generate' button is clicked
+        bingoTitle.innerHTML = titleValue;
+        boardDiv.innerHTML = promptsInput;
+
+    }
+
+   // if gridSize = "49"
     
 }
 
